bc4836554c259ad85545a4c518d3205fd486c14b
shadi <Shadim1997@gmail.com>
2020-09-24 13:55:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/market/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.market;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    private Button btnsignup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btnSignup).setOnClickListener(this);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hed4!");
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v.getId()==(R.id.btnSignup))
+            startActivity(new Intent(this,signup.class));
+    }
+}
---
+++ app/src/main/java/com/example/market/signup.java
@@ -0,0 +1,100 @@
+package com.example.market;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.market.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class signup extends AppCompatActivity implements View.OnClickListener
+{
+    EditText txtname , txtemail , txtpass,txtpass2;
+    RadioGroup rgb;
+    private FirebaseAuth mAuth;
+    DatabaseReference reff;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // t3refat
+        txtname=(EditText)findViewById(R.id.txtName);
+        txtemail=(EditText)findViewById(R.id.txtEmail2);
+        txtpass=(EditText)findViewById(R.id.txtPassword2);
+        txtpass2=(EditText)findViewById(R.id.txtPass2);
+        findViewById(R.id.btnSignup2).setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    private void registerMember() {
+        String Email = txtemail.getText().toString().trim();
+        String Name = txtname.getText().toString().trim();
+        String Pass1 = txtpass.getText().toString().trim();
+        String passw2 = txtpass2.getText().toString().trim();
+
+        if (Email.isEmpty()) {
+            txtemail.setError("Email is required");
+            txtemail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches())
+        {
+            txtemail.setError("Email is not valid!");
+            txtemail.requestFocus();
+            return;
+        }
+
+        if(Name.isEmpty())
+        {
+            txtname.setError("Name is required");
+            txtname.requestFocus();
+            return;
+        }
+        if(Pass1.isEmpty())
+        {
+            txtpass.setError("Password is required");
+            txtpass.requestFocus();
+            return;
+        }
+        if(passw2.isEmpty())
+        {
+            txtpass2.setError("Please Confirm Password");
+            txtpass2.requestFocus();
+            return;
+        }
+        if(!Pass1.equals(passw2))
+        {
+            txtpass2.setError("Password does not match!");
+            txtpass2.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(Email,Pass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(),"User Registered Successfully",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==(R.id.btnSignup2))
+            registerMember();
+    }
+}
