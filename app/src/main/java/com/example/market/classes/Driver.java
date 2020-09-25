7531cd961d2b4cf076e76db2087da9bb94809dd7
shadi <Shadim1997@gmail.com>
2020-09-26 00:14:08 +0300

    New Classes

--- app/src/main/java/com/example/market/classes/Driver.java
+++ app/src/main/java/com/example/market/classes/Driver.java
@@ -1,4 +1,23 @@
 package com.example.market.classes;
 
-public class Driver {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Driver extends User {
+   private List<Inventory> inventories=new ArrayList<Inventory>();
+
+    public List<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public Driver () {
+    }
+
+    public Driver(List<Inventory> inventories) {
+        this.inventories = inventories;
+    }
+
+    public void setInventories(List<Inventory> inventories) {
+        this.inventories = inventories;
+    }
 }
--- app/src/main/java/com/example/market/classes/Inventory.java
+++ app/src/main/java/com/example/market/classes/Inventory.java
@@ -1,4 +1,40 @@
 package com.example.market.classes;
 
 public class Inventory {
+    private Product Product;
+    private String Driver;
+    private String Quantity;
+
+    public Inventory() {
+    }
+
+    public Inventory(com.example.market.classes.Product product, String driver, String quantity) {
+        Product = product;
+        Driver = driver;
+        Quantity = quantity;
+    }
+
+    public com.example.market.classes.Product getProduct() {
+        return Product;
+    }
+
+    public void setProduct(com.example.market.classes.Product product) {
+        Product = product;
+    }
+
+    public String getDriver() {
+        return Driver;
+    }
+
+    public void setDriver(String driver) {
+        Driver = driver;
+    }
+
+    public String getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        Quantity = quantity;
+    }
 }
