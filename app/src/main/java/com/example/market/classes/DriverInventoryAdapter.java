dedab6783dc4eaf82d78bf7f3231b808f850cf14
shadi <Shadim1997@gmail.com>
2020-10-02 20:40:23 +0300

    Driver inventory 2

---
+++ app/src/main/java/com/example/market/classes/DriverInventoryAdapter.java
@@ -0,0 +1,60 @@
+package com.example.market.classes;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.market.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class DriverInventoryAdapter extends RecyclerView.Adapter<DriverInventoryAdapter.InventoryViewHolder> {
+
+    Context context;
+    ArrayList<QuanProduct> quanProducts;
+
+    public DriverInventoryAdapter(Context c , ArrayList<QuanProduct> p)
+    {
+        context=c;
+        quanProducts=p;
+    }
+
+    @NonNull
+    @Override
+    public InventoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new InventoryViewHolder(LayoutInflater.from(context).inflate(R.layout.card_view_inventory,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InventoryViewHolder holder, int position) {
+        holder.pronamein.setText(quanProducts.get(position).getName());
+        holder.propricein.setText(quanProducts.get(position).getPrice());
+        Picasso.get().load(quanProducts.get(position).getImageURL()).into(holder.proimagein);
+    }
+
+    @Override
+    public int getItemCount() {
+        return quanProducts.size();
+    }
+
+    class InventoryViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView propricein,pronamein;
+        ImageView proimagein;
+        public InventoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            pronamein = (TextView) itemView.findViewById(R.id.pronameinv);
+            propricein = (TextView) itemView.findViewById(R.id.propriceinv);
+            proimagein = (ImageView) itemView.findViewById(R.id.productPhotoinv);
+        }
+    }
+
+}
--- app/src/main/java/com/example/market/classes/QuanProduct.java
+++ app/src/main/java/com/example/market/classes/QuanProduct.java
@@ -5,6 +5,7 @@ public class QuanProduct extends Product {
 
     private int quantity=0;
 
+
     public QuanProduct(String n, String p, String i) {
         super(n, p, i);
     }
